f2523fcd33791aa718987aefbd3bd54d5ac804b0
Sheetal <sheetalsingh7007@gmail.com>
2022-12-05 04:50:29 -0600

    Setting up JS/Bootstrap for pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import Labs from "./labs";
-import HelloWorld from "./labs/a6/hello-world";
-import Tuiter from "./tuiter/index.js";
 import Animate from "./animate/index.js";
-import {BrowserRouter} from "react-router-dom";
-import {Routes, Route} from "react-router";
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route path="/*"
-                   element={<Animate/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div>
+        <h1>Hello Animators!</h1>
+        <Animate/>
+      </div>
   );
 }
 
---
+++ src/animate/index.js
@@ -0,0 +1,8 @@
+function Animate() {
+    return (
+        <div>
+            <h1>This is index page</h1>
+        </div>
+    );
+}
+export default Animate;
