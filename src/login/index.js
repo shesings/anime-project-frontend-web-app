00a067ba150b9da8c61213c63d1dd0d5d639e07f
Sheetal <sheetalsingh7007@gmail.com>
2022-12-10 22:49:13 -0500

    login form and hook for calling login api

--- src/login/index.js
+++ src/login/index.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./index.css";
+import { useLogin } from "./useLogin";
+
 const Login = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
+  const { userName, password, setPassword, setUserName, onSubmit } = useLogin();
+
+  useEffect(() => {
+  }, [userName, password]);
+
+  return (
+    <div className="login-card w-100 m-auto">
+      <h3>Signin</h3>
+      <form>
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="emailInput"
+            placeholder="name@example.com"
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            value={userName}
+          />
+          <label htmlFor="floatingInput">Email address</label>
         </div>
-    );
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+        <button onClick={onSubmit} class="w-75 btn btn-med btn-primary" type="button">
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
 };
 export default Login;
