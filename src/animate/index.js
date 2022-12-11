cf74dae37cdf000b9e7c9b9c20fcb07814fd2aa7
Sheetal <sheetalsingh7007@gmail.com>
2022-12-10 21:44:47 -0500

    replace a href to prevent page reload

--- src/animate/index.js
+++ src/animate/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Animate() {
-    let attr = this.props.attr;
     return (
         <div>
             <h1>This is index page</h1>
             <div className="col mb-6">
                 <div className="card h-100">
                 </div>
-                <div className="cardTitle"><strong>{attr.canonicalTitle}</strong></div>
+                <div className="cardTitle">
+                    <strong>{'attr.canonicalTitle'}</strong>
+                    </div>
 
             </div>
         </div>
